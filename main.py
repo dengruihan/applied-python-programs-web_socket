fa05c5bec4126743d29d2a0655dba100b7d4a860
dengruihan <101576997+dengruihan@users.noreply.github.com>
2025-10-16 13:19:48 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from http import *
+from myflask import myflask
 
-app = MiniApp()
-
-@app.route("/hello")
-def hello(request):
-  name = request.args.get("name", "Flask")
-  return f"Hello, {escape(name)}!"
+app = myflask("0.0.0.0", 8002)
 
 @app.route("/")
-def index(request):
-  return "<h1>Welcome to MiniApp</h1><p>Try /hello?name=World</p>"
- 
-app.run("0.0.0.0", 8000)
+def index():
+    return "welcome to the class"
+
+@app.route("/hello")
+def hello():
+    return "world"
+
+@app.route("/login", methods=["GET"])
+def login():
+    return '<form method="post" url="/login"><input name="username" /><input type="submit"/></form>'
+
+@app.route("/login", methods=["POST"])
+def login():
+    return 'you have submited some thing'
+
+app.run()
